95a4ce293fec749da7ac21647d81ab08f112914e
Lucas E. Oliaro Vera <lucasoliaro@gmail.com>
2023-09-30 17:30:20 -0300

    add: Read CSV as List<String[]>

--- Ejercicios/src/TP_Ideas/Lector/Lecturacsv.java
+++ Ejercicios/src/TP_Ideas/Lector/Lecturacsv.java
@@ -1,21 +1,15 @@
+package TP_Ideas.Lector;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-package TP_Ideas.Lector;
-
 public class Lecturacsv {
-
-    public static void main(String[] args) {
-        //TO DO: Change to the right expresion in the file, find the right CSV.
-        /*
-         * String currentDirectory = System.getProperty("user.dir");
-         * String csvFilePath = currentDirectory + "/src/utils/archivo.csv";
-         */
-        String csvFilePath = "ruta/al/archivo.csv"; // Reemplaza con la ruta real de tu archivo CSV
-
+     public static void main(String[] args) {
+        String csvFilePath = "Ejercicios/src/TP_Ideas/utils/industry_sic.csv";
+    
         try (BufferedReader reader = new BufferedReader(new FileReader(csvFilePath))) {
             String line;
             List<String[]> data = new ArrayList<>();
@@ -23,17 +17,21 @@ public class Lecturacsv {
             while ((line = reader.readLine()) != null) {
                 String[] row = line.split(",");
                 data.add(row);
+
+                // Inferir el tipo de dato de cada columna
+                for (String cell : row) {
+                    String dataType = Identificador.inferDType(cell);
+                    System.out.println("Tipo de dato inferido: " + dataType);
+                }
             }
 
-            // Itera sobre las filas y columnas del archivo CSV
-            for (String[] row : data) {
-                for (String cell : row) {
-                    System.out.print(cell + "\t");
-                }
-                System.out.println(); // Nueva línea para la siguiente fila
-            }
+            // Continúa con el procesamiento de los datos aquí
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
+
+
+
